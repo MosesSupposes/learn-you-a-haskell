575f28a9d9191e6932453c054ea6153bfd918d43
MosesSupposes <mosesasamuel633@gmail.com>
2021-09-09 16:09:52 -0500

    add some more IO examples

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Chapter9.InputOutput 
+import Chapter9.IOBasics 
 
 -- It's a convention in Haskell to not add a type annotation to the `main` function
 main = complementUser
--- src/Chapter9/IOBasics.hs
+++ src/Chapter9/IOBasics.hs
@@ -1,8 +1,10 @@
-module Chapter9.InputOutput
+module Chapter9.IOBasics
     ( helloChicken
     , complementUser
+    , mapSequence
     ) where
 
+import           Control.Monad
 import           Data.Char                      ( toLower
                                                 , toUpper
                                                 )
@@ -53,5 +55,56 @@ hellYeah = do
 hellYeah' :: IO ()
 hellYeah' = do
     let a = "hell"
-    let b = "yeah!"
+        b = "yeah!"
     putStrLn $ a ++ " " ++ b
+
+printUntilSpace :: IO ()
+printUntilSpace = do
+    c <- getChar
+    when (c /= ' ') $ do
+        putChar c
+        printUntilSpace
+
+
+-- The following two functions are identical.
+
+get3Lines :: IO ()
+get3Lines = do
+    a <- getLine
+    b <- getLine
+    c <- getLine
+    print [a, b, c]
+
+get3Lines' :: IO ()
+get3Lines' = do
+    ls <- sequence [getLine, getLine, getLine]
+    print ls
+
+
+-- The following two functions are identical. 
+
+mapSequence :: IO [()]
+mapSequence = sequence $ map print [1, 2, 3, 4, 5]
+
+mapSequence' :: IO [()]
+mapSequence' = mapM print [1, 2, 3, 4, 5]
+
+foreverUpperCased :: IO ()
+foreverUpperCased = forever $ do
+    putStr "Give me some input: "
+    l <- getLine
+    putStrLn $ map toUpper l
+
+pickColors :: IO [()]
+pickColors = do
+    colors <- forM
+        [1, 2, 3, 4]
+        (\a -> do
+            putStrLn
+                $  "Which color do you associate with the number "
+                ++ show a
+                ++ "?"
+            getLine
+        )
+    putStrLn "The colors that you associate with 1, 2, 3 and 4 are: "
+    mapM putStrLn colors
