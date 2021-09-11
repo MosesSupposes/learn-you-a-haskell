c183688e4a66af5f17ebba46dee42c25dce47f68
MosesSupposes <mosesasamuel633@gmail.com>
2021-09-11 16:08:26 -0500

    more examples from chapter 9

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,5 @@
 module Main where
 
-import           Chapter9.FilesAndStreams
 import           Chapter9.IOBasics              ( complementUser )
 
 -- It's a convention in Haskell to not add a type annotation to the `main` function
